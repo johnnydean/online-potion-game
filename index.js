fdfe88b1c63a9d30a4eabf71df625473559bfe66
Joseph Dean <joe@on-site.com>
2015-05-10 15:36:15 -0700

    Add socket.io and create basic chat functionality

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+app.get('/', function(req, res) {
+  res.sendfile('index.html');
 });
 
-http.listen(9001, function(){
+io.on('connection', function(socket) {
+  console.log('a user connected');
+
+  socket.on('disconnect', function() {
+    console.log('user disconnected');
+  });
+
+  socket.on('chat message', function(msg) {
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(9001, function() {
   console.log('listening on *:9001');
 });
